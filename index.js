48a4d86fc7dc925a17e17d917017f964bdd85b1c
FlPDD <103981206+FlPDD@users.noreply.github.com>
2022-04-20 02:48:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/',function(req,res){
     res.send("olá fellipe")
 })
 
-app.get('/mensagem', function(){
+app.get('/msg', function(req,res){
     res.send("Essa mensagem é automática")
 })
 
