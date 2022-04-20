0b37551d48eade6b659aa5343f45057f0941d504
Fellipe Duarte <fellipeduarte45@gmail.com>
2022-04-20 05:17:10 -0300

    tabela com função deletar

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser')
 var path = require('path')
+var Usuario = require('./model/usuario')
 
 app.use(cookieParser())
 app.use(bodyParser.json())
@@ -12,23 +13,42 @@ app.set("view engine","ejs")
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/',function(req,res){
-    res.render('index.ejs',{})
+    Usuario.find({}).exec(function(err,docs){
+        res.render('index.ejs',{Usuarios:docs})
+    })
 })
 
-app.get('/usuarios',function(req,res){
-    res.render('usuarios.ejs',{ usuarios:[
-    {nome: 'Fellipe', email:'fellipe@gmail.com'},
-    {nome: 'Marcos', email:'marcos@gmail.com'},
-    {nome: 'Diego', email:'diego@gmail.com'},
-    ]})
-})
+
 
 app.get('/add', function(req,res){
     res.render('adiciona.ejs')
 })
 
 app.post('/add', function(req,res){
-    console.log("Nome: " + req.body.txtNome + "Email: " + req.body.txtEmail)
+    var usuario = new Usuario({
+        nome: req.body.txtNome,
+        email: req.body.txtEmail,
+        senha: req.body.txtSenha,
+        
+    })
+    usuario.save(function(err){
+        if(err){
+            console.log(err)
+        } else {
+            res.redirect('/');
+        }
+    })
+})
+
+app.get('/del/:id', function(req,res){
+    Usuario.findByIdAndDelete(req.params.id, function(err){
+        if(err){
+            console.log(err)
+        }else {
+            res.redirect('/')
+        }
+    })
+
 })
 
 app.listen(3000, function(){
