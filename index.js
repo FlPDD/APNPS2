3482c1e5bc8bd2573d7d8ca43ffaddb9e4002b4c
Fellipe Duarte <fellipeduarte45@gmail.com>
2022-04-20 02:46:22 -0300

    adição da rota msg

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',function(req,res){
     res.send("olá fellipe")
 })
 
+app.get('/mensagem', function(){
+    res.send("Essa mensagem é automática")
+})
+
 app.listen(3000, function(){
     console.log("Conexão inicializada")
 })
